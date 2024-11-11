c30239bf0267c7b799ba5a2085f802a9ac8b6341
WillMcBurney <pmcburn1@gmail.com>
2024-11-11 09:56:20 -0500

    Added remaining files

---
+++ src/main/java/edu/virginia/sde/javafx/day1/HelloWorld.java
@@ -0,0 +1,26 @@
+package edu.virginia.sde.javafx.day1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        FlowPane pane = new FlowPane();
+        pane.getChildren().add(label);
+        Scene scene = new Scene(pane, 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
